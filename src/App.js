858d640dc420012485fcbcded2132684db41a2ab
Valentina Belotelova <belvalentinaa@gmail.com>
2021-08-09 17:11:20 +0300

    added collection-preview and collecton-item components with styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from 'react'
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
     </Switch>
   );
 }
